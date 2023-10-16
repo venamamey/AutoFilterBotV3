a133ca457c806cfa86c99f99c071bd5d9f50fda9
venamamey <148063962+venamamey@users.noreply.github.com>
2023-10-16 19:45:11 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,31 @@
+SESSION = 'LuciferMoringstar_Robot'
+USER_SESSION = 'User_Bot'
+API_ID = 8351599
+API_HASH = '1aa9fdfe0cb179362a1f914dcdc6065a'
+BOT_TOKEN = '6547964272:AAHOVxrl4crCkwzaTFxtEUEWclTeJQhZRE4'
+USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = [1071489732, '', ]
+CHANNELS = [-1001631431818, , '']
+AUTH_USERS = []
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "mongodb+srv://destruction:destruction@cluster0.znsccxo.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'LuciferMoringstar_Robot'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
+# Messages
+START_MSG = """
+**Hi, I'm Media Search bot**
+
+Here you can search files in inline mode. Just press follwing buttons and start searching.
+"""
+
+SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
+INVITE_MSG = 'Please join @.... to use this bot'
